34ea79ffafc2295a0e82b06645d8cea5bca10d4f
lukjanenko <lukyan-i@yandex.ua>
2014-03-05 15:19:27 +0200

    add feeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Parser::Application.routes.draw do
+  resources :feed_entries
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root "feed_entries#index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
